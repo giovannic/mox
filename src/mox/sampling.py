fd88c0f762f04d97eea78826a1af34eeaf488547
Giovanni Charles <giovanni.charles@gmail.com>
2023-06-02 09:21:45 +0100

    Add CI workflows:
     * Testing
     * Linting
     * Documentation
     * Fix sampling linting errors

--- src/mox/sampling.py
+++ src/mox/sampling.py
@@ -1,15 +1,22 @@
+"""This module handles sampling of parameter spaces.
+
+It provides abstractions for setting sampling strategies for each parameter."""
 from typing import Union, Dict, List, Any, Callable
-from scipy.stats.qmc import LatinHypercube
 from abc import ABC
 from collections.abc import Iterable
 from dataclasses import dataclass
+from scipy.stats.qmc import LatinHypercube
 from jaxtyping import PyTree, Array
 import jax.numpy as jnp
 
-class Strategy(ABC):
-    pass
+class Strategy(ABC): # pylint: disable=too-few-public-methods
+    """Strategy. Abstract base class for strategy objects"""
 
+@dataclass
 class LHSStrategy(Strategy):
+    """LHSStrategy. Strategy object for sampling a parameter from a Latin
+    Hypercube"""
+
     lower_bound: Array
     upper_bound: Array
 
@@ -24,6 +31,11 @@ class LHSStrategy(Strategy):
             raise ValueError("The lower bound should be less than or equal to the upper bound.")
 
     def transform_samples(self, samples: Array):
+        """transform_samples.
+
+        :param samples:
+        :type samples: Array
+        """
         return jnp.array(
             samples * self.upper_bound.flatten() - self.lower_bound.flatten()
                 + self.lower_bound.flatten()
@@ -36,7 +48,7 @@ ParamStrategy = Union[
     Dict[str, 'ParamStrategy']
 ]
 
-def sample(strategy: list[ParamStrategy], n: int, key) -> list[PyTree]:
+def sample(strategy: list[ParamStrategy], num: int, key) -> list[PyTree]:
     """sample. Sample from a list of parameter sampling strategies
 
     :param strategy:
@@ -48,10 +60,13 @@ def sample(strategy: list[ParamStrategy], n: int, key) -> list[PyTree]:
     """
 
     if not isinstance(strategy, Iterable):
-        raise TypeError("The strategy parameter should be iterable, such as a list or tuple, containing strategy objects.")
+        raise TypeError("""The strategy parameter should be iterable, such as a
+
+        list or tuple, containing strategy objects.""")
 
     if len(strategy) == 0:
-        raise ValueError("The strategy list should contain at least one strategy object.")
+        raise ValueError("""The strategy list should contain at least one
+        strategy object.""")
 
     # Initialise sampler for LHS strategies
     lhs_dims = jnp.array([
@@ -61,20 +76,24 @@ def sample(strategy: list[ParamStrategy], n: int, key) -> list[PyTree]:
 
     if len(lhs_dims) > 0:
         sampler = LatinHypercube(d=int(lhs_dims.sum()), seed=int(key[1]))
-        lhs_samples = _lhs_sample_generator(lhs_dims, sampler.random(n))
-        lhs_counter = 0
+        lhs_samples = _lhs_sample_generator(lhs_dims, sampler.random(num))
 
     # Create strategy sampling function
-    def sample_strategy(s):
-        if isinstance(s, LHSStrategy):
-            return s.transform_samples(next(lhs_samples))
+    def sample_strategy(strat):
+        if isinstance(strat, LHSStrategy):
+            return strat.transform_samples(next(lhs_samples))
 
-        return strategy.sample(n)
+        return strategy.sample(num)
 
     # Do the sampling for each leaf
     return _strategy_transformer(strategy, sample_strategy)
 
 def strategy_iterator(strategy: List[ParamStrategy]):
+    """strategy_iterator.
+
+    :param strategy:
+    :type strategy: List[ParamStrategy]
+    """
     stack = [strategy]
 
     while stack:
@@ -93,19 +112,23 @@ def strategy_iterator(strategy: List[ParamStrategy]):
             raise TypeError("Invalid Strategy object.")
 
 
-def _strategy_transformer(strategy: Any, f: Callable):
-    print(strategy)
+def _strategy_transformer(strategy: Any, fun: Callable):
     if isinstance(strategy, LHSStrategy):
-        return f(strategy)
+        return fun(strategy)
 
-    elif isinstance(strategy, (list, tuple)):
-        return [_strategy_transformer(sub_strategy, f) for sub_strategy in strategy]
+    if isinstance(strategy, (list, tuple)):
+        return [
+            _strategy_transformer(sub_strategy, fun)
+            for sub_strategy in strategy
+        ]
 
-    elif isinstance(strategy, dict):
-        return {key: _strategy_transformer(sub_strategy, f) for key, sub_strategy in strategy.items()}
+    if isinstance(strategy, dict):
+        return {
+            key: _strategy_transformer(sub_strategy, fun)
+            for key, sub_strategy in strategy.items()
+        }
 
-    else:
-        raise TypeError("Invalid Strategy object.")
+    raise TypeError("Invalid Strategy object.")
 
 def _lhs_sample_generator(dims: Array, samples: Array):
     current_dim = 0
